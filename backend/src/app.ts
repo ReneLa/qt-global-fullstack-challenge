c1323639f8f7187dfd6bcd8b4655f77daf4da42f
Rene La <renela79@gmail.com>
2025-10-16 05:13:59 +0200

    feat: Express server and prisma setup

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
